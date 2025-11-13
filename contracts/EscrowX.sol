955c593e9a92e92eb20bc6091933890cb4ad1820
Niharika-Amadala13 <niharika@geeksofgurukul.com>
2025-11-13 14:55:07 +0530

    Update EscrowX.sol

--- contracts/EscrowX.sol
+++ contracts/EscrowX.sol
@@ -10,7 +10,6 @@ contract SmartRoyalty is Ownable, ReentrancyGuard {
     struct Royalty {
         address recipient;
         uint96 share; 
-
     }
     // contentId => array of royalty splits
     mapping(uint256 => Royalty[]) private royalties;
